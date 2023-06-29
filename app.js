6e47e056c297d5d257bbc79269403d07317f6839
Pigeon <127341832+NebulousPigeon@users.noreply.github.com>
2023-06-29 16:18:29 +0400

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const schedule = require('node-schedule');
 
 const app = express();
 
-const links = ["https://afk-mode1.onrender.com"]; // replace with your links
+const links = ["https://afk-mode1.onrender.com", "https://vinxy-verify.onrender.com", "https://ashification-mclj.onrender.com", "https://networth-api-dxxxxy.onrender.com", "https://verification-minecraft.onrender.com", "https://veriflcation-minecraft.onrender.com", "https://verification-minecraft-153g.onrender.com", "https://modik.onrender.com"]; // replace with your links
 
 const job = schedule.scheduleJob('*/1 * * * *', function(){
   links.forEach(async (link) => {
